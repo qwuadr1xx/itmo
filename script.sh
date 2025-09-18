c7aba16a7895ea229d1cea9a14c441ad8e5fc050
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-18 16:26:35 +0500

    Update and rename lab1 to script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 mkdir lab0
 cd lab0
 
+# №1
 
 touch blaziken5
 mkdir -p golett0/electivire
@@ -28,3 +29,29 @@ echo "Ходы Bullet Seed Earth Power Giga Drain Grass Pledge Iron Tail Mud-Sla
 echo "Способности Blaze Focus Heatproof Gluttony" > murkrow9/pignite
 echo "Тип диеты Omnivore" > murkrow9/medi tite
 echo "Ходы After You Ancientpower Defense Curl Double-Edge Earth Power Heat Wave Iron Defense Mud-Slap Pain Split Rollout Sleep Talk Snore" > slugmal
+
+# №2
+
+chmod 006 blaziken5
+chmod 566 goletto0
+chmod 333 golett0/electivire
+chmod 660 golett0/pineco
+chmod 751 golett0/umbreon
+chmod 500 golett0/phanpy
+chmod 400 golett0/meganium
+chmod 622 grotle7
+chmod 700 murkrow9
+chmod 512 murkrow9/poliwrath
+chmod 317 murkrow9/duskull
+chmod 500 murkrow9/leavanny
+chmod 440 murkrow9/pignite
+chmod 753 seedot8
+chmod 576 seedot8/dragonair
+chmod 440 seedot8/meditite
+chmod 753 seedot8/kricketot
+chmod 751 seedot8/sandslash
+chmod 335 seedot8/golurk
+chmod 500 seedot8/crobat
+chmod 444 slugma1
+
+# №3
