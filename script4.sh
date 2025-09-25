2e7bc070d26dd30c918289161ea10dded2ba3807
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-25 15:48:25 +0500

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,14 +1,15 @@
 cd lab0
+mkdir tmp
 
 chmod u+rx murkrow9/duskull 2>/dev/null
 chmod u+rx golett0/electivire 2>/dev/null
 chmod u+rx seedot8/golurk 2>/dev/null
 
-wc -m d* */d* */*/d* > /tmp/result1.txt
+wc -m d* */d* */*/d* > tmp/result1.txt
 ls -lRtur murkrow9 2>&1
 cat *t */*t */*/*t 2>&1 | sort
-ls -lSRr lab0/g* lab0/*/g* lab0/*/*/g* 2>/tmp/result4.txt
-ls -lRtr 2>/tmp/result5.txt | grep -E '[^ ]*te;[^ ]*$' | tail -n 2
+ls -lSRr lab0/g* lab0/*/g* lab0/*/*/g* 2>tmp/result4.txt
+ls -lRtr 2>tmp/result5.txt | grep -E '[^ ]*te;[^ ]*$' | tail -n 2
 ls -lRtr | head -n 3
 
 chmod u-x murkrow9/duskull 2>/dev/null
