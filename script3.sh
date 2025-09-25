5c4b64238873a51794c0abf4866bbb3ee476ba41
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-25 14:18:06 +0500

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,12 @@
+cd lab0
+chmod u+wx slugma1
+ln slugma1 golett0/meganiumslugma
+chmod u-wx slugma1
+cp grotle7 golett0/umbreon
+cat golett0/meganium golett0/pineco > blaziken5_77
+cp slugma1 seedot8/medititeslugma
+chmod -R u+wx murkrow9
+cp -r murkrow9 seedot8/sandslash
+chmod -R u-wx murkrow9
+ln -s seedot8 Copy_67
+ln -s grotle7 murkrow9/pignitegrotle
