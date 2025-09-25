10ad438f61c3d9c7498f19f2f7c816bbcceb0439
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-25 15:52:33 +0500

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -8,7 +8,7 @@ chmod u+rx seedot8/golurk 2>/dev/null
 wc -m d* */d* */*/d* > tmp/result1.txt
 ls -lRtur murkrow9 2>&1
 cat *t */*t */*/*t 2>&1 | sort
-ls -lSRr lab0/g* lab0/*/g* lab0/*/*/g* 2>tmp/result4.txt
+ls -lSRr g* */g* */*/g* 2>tmp/result4.txt
 ls -lRtr 2>tmp/result5.txt | grep -E '[^ ]*te;[^ ]*$' | tail -n 2
 ls -lRtr | head -n 3
 
