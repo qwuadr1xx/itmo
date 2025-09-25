7ec9cec0b3d3abc3c1151dcf2cb6e698ebe4977a
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-25 14:59:16 +0500

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,4 +1,11 @@
 cd lab0
+
+chmod u+w golett0 2>/dev/null
+chmod u+w murkrow9 2>/dev/null
+chmod u+w seedot8 2>/dev/null
+chmod u+rx murkrow9/duskull 2>/dev/null
+
+
 ln slugma1 golett0/meganiumslugma
 cp grotle7 golett0/umbreon
 cat golett0/meganium golett0/pineco > blaziken5_77
@@ -6,3 +13,8 @@ cp slugma1 seedot8/medititeslugma
 cp -r murkrow9 seedot8/sandslash
 ln -s seedot8 Copy_67
 ln -s grotle7 murkrow9/pignitegrotle
+
+chmod u-w golett0 2>/dev/null
+chmod u-w murkrow9 2>/dev/null
+chmod u-w seedot8 2>/dev/null
+chmod u-rx murkrow9/duskull 2>/dev/null
