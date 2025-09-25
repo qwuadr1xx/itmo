d37621fca662ddfda82ec58119d68f25b221ef87
qwuadr1xx <qwuadrixx@gmail.com>
2025-09-25 14:49:29 +0500

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,8 +1,11 @@
 cd lab0
 
+chmod u+w golett0 2>/dev/null
+chmod -R u+wx golett0 2>/dev/null
+
 rm -rf blaziken5
 rm -rf murkrow9/pignite
 rm -rf Copy_*
 rm -rf golett0/meganiumslug*
 rm -rf golett0
-rmdir seedot8/kricketot
+rm -rf seedot8/kricketot 2>/dev/null
